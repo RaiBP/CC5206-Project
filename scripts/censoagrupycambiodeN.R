bcd549e14475eca4b2ea849e3aa72b1ab0d5c2d0
javie <javier.munoz.c@ing.uchile.cl>
2018-10-07 20:56:35 -0300

    script contiene metodo para agrupar el censo por comuna y cambiar el N de los datasets. Se incluyen los datasets modificados

---
+++ scripts/censoagrupycambiodeN.R
@@ -0,0 +1,12 @@
+library(readxl)
+dataper<-read_xlsx("datasets/base_2002a2020_v3.xlsx")
+datasumper<-aggregate(cbind(a2002,a2003,a2004,a2005,a2006,a2007,a2008,a2009,a2010,a2011,a2012,a2013,a2014,a2015,a2016,a2017,a2018,a2019,a2020) ~ Region + nombre_region+provincia+Nombre_provincia+Comuna+nombre_comuna,dataper,FUN=sum)
+
+for (year in 2001:2017){
+  file <- paste("C:/Users/javie/Desktop/Nueva carpeta/CC5206-Project/datasets/egr",year,".csv", sep="")
+  temp <- read.csv(file,header=T)
+  colnames(temp)[colnames(temp)=="N"] <- paste("N",year,sep="")
+  write.csv(temp,file)
+}
+
+
